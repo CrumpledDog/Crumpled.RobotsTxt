713c4e59feeb761034be74a18334f7f3dafad5b3
Jeavon Leopold <jeavon@crumpled-dog.com>
2024-02-27 13:03:19 +0000

    Empty start

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Crumpled.RobotsTxt
+
+This package adds the Crumpled Robots Txt
+
+## Install NuGet package
+
+```console
+dotnet add package Crumpled.RobotsTxt --prerelease
+```
+
+## Enable in `program.cs`
+
+```C#
+.AddCrumpledRobotsTxt()
+```
+
---
+++ src/Crumpled.RobotsTxt/Extensions/AddCrumpledRobotsTxt.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Crumpled.RobotsTxt
+{
+	public static partial class IUmbracoBuilderExtensions
+	{
+		public static IUmbracoBuilder AddCrumpledRobotsTxt(this IUmbracoBuilder builder)
+		{
+			return builder;
+		}
+	}
+}
